654f3a27a3d24e52ec34956eb405e16ed833e425
kgalb01 <k_galb01@uni-muenster.de>
2024-02-06 13:13:48 +0100

    further improvements

--- R/api.R
+++ R/api.R
@@ -175,7 +175,7 @@ NULL
           download_dir <- file.path(Sys.getenv("USERPROFILE"), "Downloads")
           tmp <- tempfile(pattern = "model", fileext = ".rds")
           file_name <- file.path(download_dir, tmp)
-          file <- saveRDS(job$results, file_name)
+          file <- base::saveRDS(job$results, file_name)
         },
         error = function(e){
           message("Error in saving data")
@@ -194,7 +194,7 @@ NULL
           download_dir <- file.path(Sys.getenv("USERPROFILE"), "Downloads")
           tmp <- tempfile(pattern = "model", fileext = ".rds")
           file_name <- file.path(download_dir, tmp)
-          file <- saveRDS(job$results, file_name)
+          file <- base::saveRDS(job$results, file_name)
         },
         error = function(e){
           message("Error in saving data")
